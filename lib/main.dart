2a5889e7f336a63d585c6f734530af6277444ade
SandroRF <sandrorf1973@gmail.com>
2023-04-17 15:06:05 -0300

    Aula - 255

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:olx/home.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    title: "OLX",
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
