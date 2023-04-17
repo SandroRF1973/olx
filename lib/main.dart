619362ebe024cba99e86e55b5dfee0309b6757c6
SandroRF <sandrorf1973@gmail.com>
2023-04-17 17:01:24 -0300

    Aula - 256

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:olx/home.dart';
 
+final ThemeData theme = ThemeData();
+
+final ThemeData temaPadrao = ThemeData().copyWith(
+    colorScheme: theme.colorScheme.copyWith(
+        primary: const Color(0xff9c27b0), secondary: const Color(0xff7b1fa2)));
+
+// final ThemeData temaPadrao = ThemeData(
+//   primaryColor: const Color(0xff9c27b0),
+// );
+
 void main() {
-  runApp(const MaterialApp(
+  runApp(MaterialApp(
     title: "OLX",
-    home: Home(),
+    home: const Home(),
+    theme: temaPadrao,
     debugShowCheckedModeBanner: false,
   ));
 }
