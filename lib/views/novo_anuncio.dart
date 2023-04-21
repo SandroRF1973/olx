bf5f25c148f2f778bc06bbce87bd323d55667d10
Sandro Ferreira <sandrorf1973@gmail.com>
2023-04-21 16:37:38 -0300

    Aula - 268

--- lib/views/novo_anuncio.dart
+++ lib/views/novo_anuncio.dart
@@ -13,6 +13,8 @@ class _NovoAnuncioState extends State<NovoAnuncio> {
   final List<File> _listaImagens = List.empty();
   final _formKey = GlobalKey<FormState>();
 
+  _selecionarImagemGaleria() {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,8 +40,51 @@ class _NovoAnuncioState extends State<NovoAnuncio> {
                   builder: (state) {
                     return Column(
                       children: [
-                        Container(),
-                        if (state.hasError) Text("[${state.errorText}]"),
+                        SizedBox(
+                          height: 100,
+                          child: ListView.builder(
+                              scrollDirection: Axis.horizontal,
+                              itemCount: _listaImagens.length + 1,
+                              itemBuilder: (context, indice) {
+                                if (indice == _listaImagens.length) {
+                                  return Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 8),
+                                    child: GestureDetector(
+                                      onTap: () {
+                                        _selecionarImagemGaleria();
+                                      },
+                                      child: CircleAvatar(
+                                        backgroundColor: Colors.grey[400],
+                                        radius: 50,
+                                        child: Column(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.center,
+                                            children: [
+                                              Icon(
+                                                Icons.add_a_photo,
+                                                size: 40,
+                                                color: Colors.grey[100],
+                                              ),
+                                              Text(
+                                                "Adicionar",
+                                                style: TextStyle(
+                                                    color: Colors.grey[100]),
+                                              ),
+                                            ]),
+                                      ),
+                                    ),
+                                  );
+                                }
+                                return Container();
+                              }),
+                        ),
+                        if (state.hasError)
+                          Text(
+                            "[${state.errorText}]",
+                            style: const TextStyle(
+                                color: Colors.red, fontSize: 14),
+                          ),
                       ],
                     );
                   },
