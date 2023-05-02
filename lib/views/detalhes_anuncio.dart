464ccaaac595ea0c9e5e274227dad24c468e054a
SandroRF <sandrorf1973@gmail.com>
2023-05-02 10:09:43 -0300

    Aula - 276

--- lib/views/detalhes_anuncio.dart
+++ lib/views/detalhes_anuncio.dart
@@ -2,6 +2,7 @@ import 'package:carousel_pro_nullsafety/carousel_pro_nullsafety.dart';
 import 'package:flutter/material.dart';
 import 'package:olx/main.dart';
 import 'package:olx/models/anuncio.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class DetalhesAnuncio extends StatefulWidget {
   Anuncio anuncio;
@@ -24,6 +25,17 @@ class _DetalhesAnuncioState extends State<DetalhesAnuncio> {
                 image: NetworkImage(url), fit: BoxFit.fitWidth)),
       );
     }).toList();
+  }
+
+  _ligarTelefone(String telefone) async {
+    Uri telefoneUri = Uri.parse("tel:$telefone");
+
+    if (await canLaunchUrl(telefoneUri)) {
+      await launchUrl(telefoneUri);
+    } else {
+      // ignore: avoid_print
+      print("Não pode fazer a ligação");
+    }
   }
 
   @override
@@ -52,9 +64,79 @@ class _DetalhesAnuncioState extends State<DetalhesAnuncio> {
                   autoplay: false,
                   dotIncreasedColor: temaPadrao.primaryColor,
                 ),
+              ),
+              Container(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      _anuncio.preco,
+                      style: TextStyle(
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                          color: temaPadrao.primaryColor),
+                    ),
+                    Text(
+                      _anuncio.titulo,
+                      style: const TextStyle(
+                          fontSize: 25, fontWeight: FontWeight.w400),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 16),
+                      child: Divider(),
+                    ),
+                    const Text(
+                      "Descrição",
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      _anuncio.descricao,
+                      style: const TextStyle(fontSize: 18),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 16),
+                      child: Divider(),
+                    ),
+                    const Text(
+                      "Contato",
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 66),
+                      child: Text(
+                        _anuncio.telefone,
+                        style: const TextStyle(fontSize: 18),
+                      ),
+                    ),
+                  ],
+                ),
               )
             ],
-          )
+          ),
+          Positioned(
+            left: 16,
+            right: 16,
+            bottom: 16,
+            child: GestureDetector(
+              child: Container(
+                padding: const EdgeInsets.all(16),
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                    color: temaPadrao.primaryColor,
+                    borderRadius: BorderRadius.circular(30)),
+                child: const Text(
+                  "Ligar",
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+              ),
+              onTap: () {
+                _ligarTelefone(_anuncio.telefone);
+              },
+            ),
+          ),
         ],
       ),
     );
